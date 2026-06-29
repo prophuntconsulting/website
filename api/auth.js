6eb148ab185a2e096483c047ccdbfee7b0e0099f
prophuntconsulting <297388630+prophuntconsulting@users.noreply.github.com>
2026-06-29 16:16:08 +0530

    Normalize canonical URLs to www domain

--- api/auth.js
+++ api/auth.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   const params = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID,
     scope: 'repo,user',
-    redirect_uri: 'https://prophuntllp.com/api/callback'
+    redirect_uri: 'https://www.prophuntllp.com/api/callback'
   });
   res.redirect(302, `https://github.com/login/oauth/authorize?${params}`);
 };
